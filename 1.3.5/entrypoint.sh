927ca981e691611c828048f4b0c6270230acc10c
Allan Lei <allanlei@helveticode.com>
2016-08-30 14:13:01 +0800

    Create ca.crt in /tmp

--- 1.3.5/entrypoint.sh
+++ 1.3.5/entrypoint.sh
@@ -11,9 +11,9 @@ if [ ! -z "$KUBE_CONTEXT" ]; then
     fi
 
     if [ ! -z "$KUBE_CLUSTER_CA" ]; then
-        echo $KUBE_CLUSTER_CA | base64 -d > ca.crt && \
-        kubectl config set-cluster $KUBE_CONTEXT --certificate-authority ca.crt --embed-certs && \
-        rm ca.crt
+        echo $KUBE_CLUSTER_CA | base64 -d > /tmp/ca.crt && \
+        kubectl config set-cluster $KUBE_CONTEXT --certificate-authority /tmp/ca.crt --embed-certs && \
+        rm /tmp/ca.crt
     fi
 
     kubectl config set-context $KUBE_CONTEXT --cluster $KUBE_CONTEXT --user $KUBE_CONTEXT && \
