09d7d424288343fe5d807b66567eedaafa448cfe
Allan Lei <allanlei@helveticode.com>
2016-08-30 11:47:03 +0800

    Make CA and endpoint exclusive

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,13 +7,13 @@ if [ ! -z "$KUBE_CONTEXT" ]; then
     fi
 
     if [ ! -z "$KUBE_CLUSTER_ENDPOINT" ]; then
-        if [ ! -z "$KUBE_CLUSTER_CA" ]; then
-            echo $KUBE_CLUSTER_CA | base64 -d > ca.crt && \
-            kubectl config set-cluster $KUBE_CONTEXT --certificate-authority ca.crt --embed-certs --server $KUBE_CLUSTER_ENDPOINT && \
-            rm ca.crt
-        else
-            kubectl config set-cluster $KUBE_CONTEXT --server $KUBE_CLUSTER_ENDPOINT
-        fi
+        kubectl config set-cluster $KUBE_CONTEXT --server $KUBE_CLUSTER_ENDPOINT
+    fi
+
+    if [ ! -z "$KUBE_CLUSTER_CA" ]; then
+        echo $KUBE_CLUSTER_CA | base64 -d > ca.crt && \
+        kubectl config set-cluster $KUBE_CONTEXT --certificate-authority ca.crt --embed-certs && \
+        rm ca.crt
     fi
 
     kubectl config set-context $KUBE_CONTEXT --cluster $KUBE_CONTEXT --user $KUBE_CONTEXT && \
